a45fc1f6b51f4b883f6236fb79ccc7eaaec1f22e
cwhillock-cmu <cwhilloc@andrew.cmu.edu>
2026-05-19 19:50:55 -0400

    new eos almost working, needs viscosity and more mass props

--- co2_eor/tests/test_EOS.py
+++ co2_eor/tests/test_EOS.py
@@ -1,31 +1,34 @@
 import pyomo.environ as pyo
 import idaes.core as idaescore
 import pyomo.util as pyoutil
-import idaes.models.properties.general_helmholtz as idaesHelmholtz
 import numpy as np
+from idaes.models.properties.modular_properties.base.generic_property import GenericParameterBlock
+from co2_eor.thermo_config import configuration
 
 m = pyo.ConcreteModel()
 m.fs = idaescore.FlowsheetBlock(dynamic=False)
-m.fs.paramBlock = idaesHelmholtz.HelmholtzParameterBlock(
-        pure_component="CO2",amount_basis=idaesHelmholtz.AmountBasis.MASS,
-        state_vars=idaesHelmholtz.StateVars.TPX,phase_presentation=idaesHelmholtz.PhaseType.MIX
-        )
+m.fs.paramBlock = GenericParameterBlock(**configuration)
 
 m.fs.stateBlock1 = m.fs.paramBlock.build_state_block(defined_state=True)
-
+m.pprint()
 m.fs.stateBlock1.flow_mass.fix(100)
-m.fs.stateBlock1.temperature.fix(273.15+36.75)
+m.fs.stateBlock1.temperature.fix(298.15)
+
 #m.fs.stateBlock1.pressure.fix(73*100000)
 
 #m.fs.stateBlock1.initialize()
 #print(f'mass density: {pyo.value(m.fs.stateBlock1.dens_mass)}')
 #print(f'cp mass: {pyo.value(m.fs.stateBlock1.cp_mass)}')
+solver = pyo.SolverFactory('ipopt')
 
 plist = np.linspace(75,90,30)
 
 for p in plist:
-    m.fs.stateBlock1.pressure=p*100000
-    m.fs.stateBlock1.vapor_frac=0.5
+    m.fs.stateBlock1.pressure.fix(p*100000)
     m.fs.stateBlock1.initialize()
-    #print(f'pressure:{p:.2f},density:{pyo.value(m.fs.stateBlock1.dens_mass):.2f},Tcrit:{pyo.value(m.fs.stateBlock1.temperature_crit):.2f},Pcrit:{pyo.value(m.fs.stateBlock1.pressure_crit)/100000:.2f},Tsat:{pyo.value(m.fs.stateBlock1.temperature_sat):.2f},Psat:{pyo.value(m.fs.stateBlock1.pressure_sat)/100000:.2f},L Visc:{pyo.value(m.fs.stateBlock1.visc_d_phase["Liq"])},G Visc:{pyo.value(m.fs.stateBlock1.visc_d_phase["Vap"])},x:{pyo.value(m.fs.stateBlock1.vapor_frac)}, L sos:{pyo.value(m.fs.stateBlock1.speed_sound_phase["Liq"]):.2f}, V sos: {pyo.value(m.fs.stateBlock1.speed_sound_phase["Vap"]):.2f}')
-    print(f'pressure:{p:.2f},Cp: {pyo.value(m.fs.stateBlock1.cp_mass):.2f} Cp/Cv: {pyo.value(m.fs.stateBlock1.heat_capacity_ratio):.2f}')
+    res = solver.solve(m)
+    print(f'pressure:{p:.2f},density:{pyo.value(m.fs.stateBlock1.dens_mass):.2f}')
+    #print(f'pressure:{p:.2f},Cp: {pyo.value(m.fs.stateBlock1.cp_mol):.2f} Cp/Cv: {pyo.value(m.fs.stateBlock1.heat_capacity_ratio):.2f}')
+    print(pyo.value(m.fs.stateBlock1.mw))
+    print(pyo.value(m.fs.stateBlock1.enth_mol))
+    print(pyo.value(m.fs.stateBlock1.visc_d_phase["Liq"]))
---
+++ co2_eor/thermo_config.py
@@ -0,0 +1,81 @@
+from pyomo.environ import units as units
+from idaes.core import Component, PhaseType, LiquidPhase, VaporPhase
+from idaes.models.properties.modular_properties.state_definitions import FTPx
+from idaes.models.properties.modular_properties.eos.ceos import Cubic, CubicType
+from idaes.models.properties.modular_properties.pure import NIST
+from idaes.models.properties.modular_properties.pure import ChungViscosityPure
+from idaes.models.properties.modular_properties.pure.ChapmanEnskog import collision_integral_neufeld_callback
+from idaes.models.properties.modular_properties.transport_properties import ViscosityWilke, NoMethod
+from idaes.models.properties.modular_properties.transport_properties.viscosity_wilke import wilke_phi_ij_callback, herring_zimmer_phi_ij_callback
+from co2_eor import mFTPx, mFPhx
+
+configuration = {
+    "base_units":{
+        "time":units.s,
+        "length":units.m,
+        "mass":units.kg,
+        "amount":units.mol,
+        "temperature":units.K,
+    },
+    "components":{
+        "co2":{
+            "type":Component,
+            "elemental_composition":{"C":1,"O":2},
+            "enth_mol_ig_comp":NIST,
+            "entr_mol_ig_comp":NIST,
+            "cp_mol_ig_comp":NIST,
+            "pressure_sat_comp":NIST,
+            "valid_phase_types":PhaseType.liquidPhase,
+            "visc_d_phase_comp":{"Liq":ChungViscosityPure},
+            #"viscosity_collision_integral_callback":collision_integral_neufeld_callback,
+            "parameter_data":{
+                "mw":(44.009e-3,units.kg/units.mol),
+                "pressure_crit":(73.825*100000,units.Pa),
+                "temperature_crit":(304.23,units.K),
+                "omega":0.225,
+                "compress_fact_crit":0.275,
+                "dens_mol_crit":(10590,units.mol/units.m**3),
+                "cp_mol_ig_comp_coeff": {
+                    "A":(24.99735,units.J/units.mol/units.K),
+                    "B":(55.18696,units.J/units.mol/units.K**2),
+                    "C":(-33.69137,units.J/units.mol/units.K**3),
+                    "D":(7.948387,units.J/units.mol/units.K**4),
+                    "E":(-0.136638,units.J*units.K**2/units.mol/units.K),
+                    "F":(-403.6075,units.kJ/units.mol),
+                    "G":(228.2431,units.J/units.mol/units.K),
+                    "H":(-393.5224,units.kJ/units.mol),
+                },
+                "dipole_moment":(0,units.debye),
+                "association_factor_chung":0,
+                "pressure_sat_comp_coeff":{
+                        "A":6.81228,
+                        "B":(1301.679,units.K),
+                        "C":(-3.494,units.K),
+                    },
+                "entr_mol_form_liq_comp_ref":(0,units.J/units.mol/units.K), #double check this
+                "enth_mol_form_liq_comp_ref":(0,units.J/units.mol), #double check this
+            },
+        },
+    },
+    "phases":{
+        "Liq":{
+            "type":LiquidPhase,
+            "equation_of_state":Cubic,
+            "equation_of_state_options":{"type":CubicType.PR},
+            "visc_d_phase": NoMethod,
+        },
+    },
+    "state_definition":mFTPx,
+    "state_bounds":{
+        "flow_mass":(-20000,1,20000,units.kg/units.s),
+        "temperature":(216,298,1500,units.K),
+        "pressure":(5e4,80*100000,2000*100000,units.Pa)
+    },
+    "pressure_ref":(101325,units.Pa), #double check this
+    "temperature_ref":(298.15,units.K), #double check this
+    "parameter_data":{
+        "PR_kappa":{
+            ("co2","co2"):0,
+        }
+    }
+}
